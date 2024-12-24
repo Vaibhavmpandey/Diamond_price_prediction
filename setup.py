880a96c781869f807268bbfccc9be96720492a08
vaibhav <vaibhavmpandey10@gmail.com>
2024-12-24 19:45:27 +0530

    Updated requirements.txt

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='DimondPricePrediction',
+    version='0.0.1',
+    author='vaibhav pandey',
+    author_email='vaibhavmpandey10@gmail.com',
+    #install_requires=["scikit-learn","pandas","numpy"],
+    packages=find_packages()
+)
